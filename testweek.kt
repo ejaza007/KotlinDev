e41b3309388b27271ec217f3e819a6528591b9f0
Armaghan Ejaz <ejaza@lafayette.edu>
2023-06-18 17:43:52 +0500

    Module 1
    module 1 test

---
+++ testweek.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+
+    if (age < 13)
+        return 15
+    else if (age in 14..59)
+        return 25
+    else if (age > 61)
+        return 20
+    else
+        return -1;
+
+}
+
+
