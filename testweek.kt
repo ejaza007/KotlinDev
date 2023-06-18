a910412cb3744d6dc8d5ce5173c2a95243df6fe7
Armaghan Ejaz <ejaza@lafayette.edu>
2023-06-18 18:16:44 +0500

    Temp Class test

--- testweek.kt
+++ testweek.kt
@@ -1,26 +1,27 @@
 fun main() {
-    val child = 5
-    val adult = 28
-    val senior = 87
-
-    val isMonday = true
-
-    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
-    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
-    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+    printFinalTemperature(27.0, "Celsius", "Fahrenheit") { celsiusToFahrenheit(it) }
+    printFinalTemperature(350.0, "Kelvin", "Celsius") { kelvinToCelsius(it) }
+    printFinalTemperature(10.0, "Fahrenheit", "Kelvin") { fahrenheitToKelvin(it) }
 }
 
-fun ticketPrice(age: Int, isMonday: Boolean): Int {
-
-    if (age < 13)
-        return 15
-    else if (age in 14..59)
-        return 25
-    else if (age > 61)
-        return 20
-    else
-        return -1;
-
+fun celsiusToFahrenheit(celsius: Double): Double {
+    return (9.0 / 5.0 * celsius) + 32.0
 }
 
+fun kelvinToCelsius(kelvin: Double): Double {
+    return kelvin - 273.15
+}
 
+fun fahrenheitToKelvin(fahrenheit: Double): Double {
+    return (5.0 / 9.0 * (fahrenheit - 32.0)) + 273.15
+}
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+}
